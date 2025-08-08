3379a28b1aab5457afbcbdfdd72c76246f0c9617
Mike Santora <santoram@oregonstate.edu>
2025-08-07 22:07:41 -0400

    8.7 Created and first commit

---
+++ 1RMX_Calc.py
@@ -0,0 +1,27 @@
+import json
+from flask import Flask, jsonify, request, Response
+
+app = Flask(__name__)
+
+print('server listening on port 5004...')
+
+@app.route('/1rm', methods=['GET'])
+def get_exercise_info():
+    """
+    calculates 1 rep max from weight and reps provided
+    """
+    try:
+        weight_request = int(request.args.get('weight'))
+        reps_request = int(request.args.get('reps'))
+        one_rep_max = int(weight_request /(1.0278 - 0.0278 * reps_request))
+        return Response(json.dumps(f"Your one rep max is {one_rep_max}"))
+    except:
+        print(f"Error: Unable to calculate")
+        return jsonify({"error": "Could not calculate 1 rep max"}),500
+
+
+
+if __name__ == '__main__':
+    # check to make sure the file path exsists before execution
+    # note this is running on port 5004
+    app.run(host='127.0.0.1', port=5004, debug=True)
